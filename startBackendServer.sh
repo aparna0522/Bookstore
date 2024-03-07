9825b893278b621632445db3d445add7eef65ab3
Aparna Naik <aparna0522@gmail.com>
2024-03-06 16:37:38 -0800

    Readme added

---
+++ startBackendServer.sh
@@ -0,0 +1,6 @@
+# Installs the required dependencies for the backend and starts the backend server, at port 8080
+npm install express
+npm install cors
+npm install mongoose
+npm install nodemon
+npm run dev
---
+++ startFrontendServer.sh
@@ -0,0 +1,3 @@
+# To connect the backend with the frontend we need a frontend server to serve our backend. 
+# The backend is serving at port 8080, and the frontend by default will start at 8081
+http-server
